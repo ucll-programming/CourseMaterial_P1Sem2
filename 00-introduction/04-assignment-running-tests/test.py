08b29d1cd0f3dbf8ea9127f22b9d3eecdd3cadcc
karenbaerts <70140404+karenbaerts@users.noreply.github.com>
2024-02-09 14:48:23 +0100

    Update test.py

--- 00-introduction/04-assignment-running-tests/test.py
+++ 00-introduction/04-assignment-running-tests/test.py
@@ -4,4 +4,4 @@ def test_script(capsys):
     captured = capsys.readouterr()
     output = captured.out
 
-    assert output == 'Hello!\n', f"Expected output is 'Hello!', instead you printed 'Hello'"
+    assert output == 'Hello!\n', f"Expected output is 'Hello!', instead you printed {output}"
