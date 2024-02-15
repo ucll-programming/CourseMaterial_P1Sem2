10cd8d91b54843a323bb28975a7a4e17452c1dae
Karen Baerts <karen.baerts@ucll.be>
2024-02-15 09:32:19 +0100

    fout 02-03 test.py

--- 02-booleans/03-assignment-free-ticket/test.py
+++ 02-booleans/03-assignment-free-ticket/test.py
@@ -3,22 +3,15 @@ import student
 
 
 @pytest.mark.timeout(1)
-@pytest.mark.parametrize('x, y', [
-    (x, x + sign * dx)
-    for x in [-51, -0.2, 0, 0.6, 1, 4.9, 2431.1]
-    for dx in [0, 0.09, 0.099]
-    for sign in [-1, 1]
+@pytest.mark.parametrize('age', [
+    *range(0, 12),
+    *range(65, 200),
 ])
-def test_close_enough(x, y):
-    assert student.close_enough(x, y), f'{x} and {y} should be considered close enough'
+def test_free_ticket(age):
+    assert student.free_ticket(age)
 
 
 @pytest.mark.timeout(1)
-@pytest.mark.parametrize('x, y', [
-    (x + sign * dx, x)
-    for x in [-51, -0.2, 0, 0.6, 1, 4.9, 2431.1]
-    for dx in [0.11, 0.15, 0.8, 1, 12, 19, 500]
-    for sign in [-1, 1]
-])
-def test_not_close_enough(x, y):
-    assert not student.close_enough(x, y), f'{x} and {y} should be considered not close enough'
+@pytest.mark.parametrize('age', range(12, 65))
+def test_no_free_ticket(age):
+    assert not student.free_ticket(age)
